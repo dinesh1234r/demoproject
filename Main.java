edf66c39307fd06feaa5f9df2a295e7f62c7fe20
DINESHKUMAR R <107352596+dinesh1234r@users.noreply.github.com>
2024-11-27 20:39:45 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,6 +2,6 @@ public class Main {
     public static void main(String[] args)
     {
         person p=new person("dinesh",12);
-        System.out.println(p.name+" "+p.age);
+        System.out.println(p.name+" "+p.age+" hello");
     }
 }
